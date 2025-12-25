45543c04f8ff4275ae8d120785c7489c2e5215c8
HaoNaN <1207185592@qq.com>
2025-12-25 21:00:03 +0800

    修复编码问题

--- _plugins/github_wiki_include.rb
+++ _plugins/github_wiki_include.rb
@@ -13,10 +13,11 @@ module Jekyll
     end
 
     def render(context)
-      # 获取原始 Markdown 内容
       raw_content = fetch_wiki_content
       
-      # 使用 Jekyll 的 Markdown 转换器
+      # 确保内容编码为 UTF-8
+      raw_content = ensure_utf8(raw_content)
+      
       site = context.registers[:site]
       converter = site.find_converter_instance(Jekyll::Converters::Markdown)
       converter.convert(raw_content)
@@ -36,6 +37,10 @@ module Jekyll
         http.use_ssl = true if uri.scheme == 'https'
         
         request = Net::HTTP::Get.new(uri.request_uri)
+        
+        # 设置接受编码
+        request['Accept-Charset'] = 'UTF-8'
+        
         response = http.request(request)
         
         if response.code == '200'
@@ -45,6 +50,26 @@ module Jekyll
         end
       rescue => e
         "获取 Wiki 内容时出错: #{e.message}"
+      end
+    end
+    
+    def ensure_utf8(str)
+      # 如果字符串已经编码为 UTF-8，直接返回
+      return str if str.encoding == Encoding::UTF_8 && str.valid_encoding?
+      
+      # 尝试检测并转换编码
+      begin
+        # 先尝试强制转换为 UTF-8，替换无效字符
+        str = str.force_encoding('UTF-8')
+        unless str.valid_encoding?
+          # 如果是 ASCII-8BIT (二进制)，尝试用不同编码检测
+          str = str.force_encoding('ASCII-8BIT').encode('UTF-8', invalid: :replace, undef: :replace)
+        end
+        str
+      rescue Encoding::UndefinedConversionError => e
+        # 如果转换失败，使用更安全的方法
+        str = str.encode('UTF-8', 'binary', invalid: :replace, undef: :replace)
+        str
       end
     end
   end
